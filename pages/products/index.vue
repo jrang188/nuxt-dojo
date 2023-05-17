8d1deea77247394fc4dc4d3a0ee2d7ff77328da1
Justin Ang <jrang@sfu.ca>
2023-05-16 18:10:46 -0700

    nuxt fetch

---
+++ pages/products/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div class="grid grid-cols-4 gap-5">
+      <div v-for="p in products">
+        <NuxtLink :to="`/products/${p.id}`">{{ p.title }}</NuxtLink>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+  layout: "products",
+});
+
+// Fetch products from the API
+const { data: products } = await useFetch("https://fakestoreapi.com/products");
+</script>
+
+<style scoped></style>
